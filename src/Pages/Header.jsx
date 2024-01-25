fd68016c9463d162dc11f63c90acfd9e94d30f1b
rezvirayhan <devrezvi.rayhan@gmail.com>
2024-01-25 17:01:22 +0600

    Add to Cart modal done

--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,10 +1,20 @@
+import { useState } from 'react';
 import Moon from '../assets/icons/moon.svg';
 import Logo from '../assets/logo.svg';
 import Ring from '../assets/ring.svg';
 import Shopping_Card from '../assets/shopping-cart.svg';
+import CartDetails from '../cine/CartDetails';
 const Header = () => {
+    const [showCart, setShowCart] = useState(false)
+    function handleCartShow() {
+
+        setShowCart(true)
+    }
     return (
         <header>
+            {
+                showCart && <CartDetails onClose={() => setShowCart(false)} />
+            }
             <nav className="container flex items-center justify-between space-x-10 py-6">
                 <a href="index.html">
                     <img src={Logo} width="139" height="26" alt="Logo" />
@@ -22,7 +32,7 @@ const Header = () => {
                         </a>
                     </li>
                     <li>
-                        <a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
+                        <a onClick={handleCartShow} className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
                             <img src={Shopping_Card} width="24" height="24" alt="Cart" />
                         </a>
                     </li>
